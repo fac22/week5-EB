0e25d7c068199baba9965e5042a386a51f2745ee
Elena <rhapsodyenmauve@gmail.com>
2021-10-27 11:30:48 +0100

    eslint + prettier + create React project
    Co-authored-by: Barbara <77367593+0bubbles0@users.noreply.github.com>

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+  },
+  plugins: ['react'],
+  rules: {},
+};
