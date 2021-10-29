e9b1527fe293325c5e18c505adc2d14da2b4767f
Elena <rhapsodyenmauve@gmail.com>
2021-10-29 12:15:23 +0100

    tried initialising state with a function - did not work
    Co-authored-by: Barbara <77367593+0bubbles0@users.noreply.github.com>

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from 'react';
 import './card.css';
 import BerryStatus from './berry-status';
 
-
 function Card(props) {
   const [berry, setBerry] = useState(0);
+  // const [berry, setBerry] = useState(() => {
+  //   console.log(berry);
+  //   localStorage.getItem(parseInt(`${props.user.id}`));
+  // });
   const [flipped, setFlipped] = useState('');
 
   useEffect(() => {
@@ -12,48 +15,57 @@ function Card(props) {
   }, []);
 
   useEffect(() => {
-    window.localStorage.setItem(`${props.user.id}${props.category}`, berry);
+    window.localStorage.setItem(`${props.user.id}`, berry);
   }, [berry]);
 
   const increaseBerry = () => {
     return setBerry(berry + 1);
-  }
+  };
   const decreaseBerry = () => {
-    return setBerry(berry - 1)
-  }
+    return setBerry(berry - 1);
+  };
 
   return (
     <article className={`card-container ${flipped}`}>
-  
-    <div className="card card-a">
-      <button className='flip-btn' onClick={() => setFlipped('flipped')}>Surprise 😉</button> 
+      <div className="card card-a">
+        <button className="flip-btn" onClick={() => setFlipped('flipped')}>
+          Surprise 😉
+        </button>
 
-      <img
-        src={`https://robohash.org/${props.user.id}?set=set${props.category}&size=180x180`}
-        alt="user"
+        <img
+          src={`https://robohash.org/${props.user.id}?set=set${props.category}&size=180x180`}
+          alt="user"
         />
-      <h2>{props.user.name}</h2>
-
-      <div className="button-flex">
-        <button onClick={decreaseBerry}>🍓 -</button>
-        <p>Score: {berry} 🍓</p>
-        <button onClick={increaseBerry}>+ 🍓</button>
-      </div>
-    </div>
-
-
-    <div className="card card-b">
-      <button className='flip-btn' onClick={() => setFlipped('')}>Feed me more 🍓 !!!</button>
-
-      <div>
         <h2>{props.user.name}</h2>
-        <p>My website <a href={`https://${props.user.website}`}>{props.user.website}</a></p>
-        <BerryStatus berry={berry} username={props.user.username} city={props.user.address.city} company={props.user.company.name}/>
-      </div>
-    </div>
 
+        <div className="button-flex">
+          <button onClick={decreaseBerry}>🍓 -</button>
+          <p>Score: {berry} 🍓</p>
+          <button onClick={increaseBerry}>+ 🍓</button>
+        </div>
+      </div>
+
+      <div className="card card-b">
+        <button className="flip-btn" onClick={() => setFlipped('')}>
+          Feed me more 🍓 !!!
+        </button>
+
+        <div>
+          <h2>{props.user.name}</h2>
+          <p>
+            My website{' '}
+            <a href={`https://${props.user.website}`}>{props.user.website}</a>
+          </p>
+          <BerryStatus
+            berry={berry}
+            username={props.user.username}
+            city={props.user.address.city}
+            company={props.user.company.name}
+          />
+        </div>
+      </div>
     </article>
   );
 }
 
-export default Card
+export default Card;
